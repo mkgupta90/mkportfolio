05ed1af29e25ed3c00b0ffd5d11154ca94e6cea5
Munna Kumar <munnakumar727798@gmail.com>
2021-03-19 17:44:34 +0530

    Text Change

--- src/components/Works/index.js
+++ src/components/Works/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const Works = () => {
     return (
         <div className="works">
-            <h1>Expricence</h1>
+            <h1>I Find A Job</h1>
             <p>
             I am a Fresher but I have Knowledge and Better Understanding about <span>Reactjs</span>,<span> Nodejs</span> , <span> Express</span> and <span>MongoDB</span>.
             </p>
